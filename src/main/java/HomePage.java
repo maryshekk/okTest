888ebd585314af734c7f5f57b1e1f95876400deb
Мария <mashastik2013@,ail.tu>
2022-10-25 20:29:19 +0300

    homepage

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePage {
@@ -14,5 +15,13 @@ public class HomePage {
         String title = driver.getTitle();
         return title.equals("Одноклассники")
                 && driver.findElement(By.xpath("//*[@id='viewImageLinkId']")).isEnabled();
+    }
+
+    public void findPerson()
+    {
+        driver.findElement(By.xpath("//button[contains(@class,'button-clean')]")).click(); //нажать на поисr
+        driver.findElement(By.xpath("//button[text()='Люди'])")).click(); //вкладка люди
+        driver.findElement(By.xpath("//*[@placeholder='Введите запрос']")).sendKeys("technoPol22" + Keys.ENTER);
+        driver.findElement(By.xpath("//span[text()='Добавить в друзья']")).click(); //кликнуть на добавить в друзья
     }
 }
